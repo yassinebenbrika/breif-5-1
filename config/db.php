a3732f92fa6156b2cc8ad56106bf2e0ed02dc351
yassinebenbrika <freudsigmund437@gmail.com>
2023-11-17 11:27:58 +0100

    upload files

---
+++ config/db.php
@@ -0,0 +1,6 @@
+<?php
+ 
+ $con =mysqli_connect("localhost","root","","mydatabase");
+ if(!$con){
+    die("connection error");
+ }
---
+++ member.php
@@ -0,0 +1,77 @@
+
+<?php
+require_once('config/db.php');
+$query = "SELECT * FROM member inner join equipe on member.equipe = equipe.id_equipe";
+
+$result = mysqli_query($con, $query);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>table</title>
+</head>
+<body>
+    <div class="bg-gray-100 py-10">
+        <div class="px-4 sm:px-6 lg:px-8">
+            <div class="sm:flex sm:items-center">
+                <div class="sm:flex-auto">
+                    <h1 class="text-xl font-semibold text-gray-900">Members</h1>
+                    <p class="mt-2 text-sm text-gray-700">A list of all the members in our company including their fullName, title, phoneNumber, email, status, its team, and role.</p>
+                </div>
+                <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                    <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add user</button>
+                </div>
+            </div>
+            <div class="mt-8 flex flex-col">
+                <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle">
+                        <div class="overflow-hidden shadow-sm ring-1 ring-black ring-opacity-5">
+                            <table class="min-w-full divide-y divide-gray-300">
+                                <thead class="bg-gray-50">
+                                    <tr>
+                                        <th scope="col" class="py-3.5 pl-4 pr-3 text-center text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">id </th>
+                                        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">phoneNumber</th>
+                                        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">firstName</th>
+                                        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">fullName</th>
+                                        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">email</th>
+                                        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">role</th>
+                                        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">team</th>
+                                        <th scope="col" class="px-3 py-3.5 text-center text-sm font-semibold text-gray-900">status</th>
+                                        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6 lg:pr-8">
+                                            <span class="sr-only">Edit</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                    ?>
+                                        <tr>
+                                            <td class="text-center"><?php echo $row['id'] ?></td>
+                                            <td class="text-center"><?php echo $row['telephone'] ?></td>
+                                            <td class="text-center"><?php echo $row['nom'] ?></td>
+                                            <td class="text-center"><?php echo $row['prenom'] ?></td>
+                                            <td class="text-center"><?php echo $row['email'] ?></td>
+                                            <td class="text-center"><?php echo $row['role'] ?></td>
+                                            <td class="text-center"><?php echo $row['equipe'] ?></td>
+                                            <td class="text-center"><?php echo $row['statut'] ?></td>
+                                             <td class="text-center"><?php echo $row['nom_de_equipe'] ?></td>
+                                            <td class="text-center"><?php echo $row['date_de_creation'] ?></td>
+                                        </tr>
+                                    <?php
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
